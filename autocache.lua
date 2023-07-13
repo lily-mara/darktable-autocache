c4b763bb7543c4798c3ed4032ebda06d96423527
Lily Mara <lilymara@fastmail.com>
2023-07-13 08:45:13 -0700

    job

--- autocache.lua
+++ autocache.lua
@@ -9,7 +9,20 @@ local function destroy()
     -- nothing to destroy
 end
 
-dt.print("hello, world from autocache")
+local image_count_current
+local image_count_total
+local autocache_progress
+local is_in_progress
+
+dt.register_event('pre-import-autocache', 'pre-import', function(_event, images)
+    image_count_total = #images
+    image_count_current = 0
+    is_in_progress = true
+    autocache_progress = dt.gui.create_job('Import autocache', true, function()
+        is_in_progress = false
+        autocache_progress.valid = false
+    end)
+end)
 
 -- set the destroy routine so that script_manager can call it when
 -- it's time to destroy the script and then return the data to
